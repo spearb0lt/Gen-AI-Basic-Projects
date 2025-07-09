7c8d8b1ea443acfdc463556bfa85d2f086b6e077
Shubhro Dev <84016735+spearb0lt@users.noreply.github.com>
2025-07-09 09:42:06 +0530

    Update app.py

--- Search-Engine-LLM/app.py
+++ Search-Engine-LLM/app.py
@@ -29,6 +29,33 @@ Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/str
 st.sidebar.title("Settings")
 api_key=st.sidebar.text_input("Enter your Groq API Key:",type="password")
 
+
+
+
+
+# Resolve literal “master” alias
+if api_key.strip().lower() == "master":
+    # api_key = "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"
+    api_key=st.secrets["GROQ_API_KEY"]
+    api_key= secets.GROQ_API_KEY
+
+else:
+    api_key = api_key.strip()
+
+if not api_key:
+    st.error("Please enter your Groq API key")
+    st.stop()
+
+
+
+
+
+
+
+
+
+
+
 if "messages" not in st.session_state:
     st.session_state["messages"]=[
         {"role":"assisstant","content":"Hi,I'm a chatbot who can search the web. How can I help you?"}
